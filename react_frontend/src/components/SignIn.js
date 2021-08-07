1ed96f027c8e758aacd94c6f255ae35c55f0880b
salalem <mohsam97@gmail.com>
2021-08-07 18:14:20 +0300

    restyling signUp page

--- react_frontend/src/components/SignIn.js
+++ react_frontend/src/components/SignIn.js
@@ -2,48 +2,95 @@ import React, {useState} from 'react';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import cookie from 'js-cookie';
+import {Container, Grid, Paper, Typography} from "@material-ui/core";
 
 
 const SignIn = (props) => {
-    const {setIsAuthorized, setSignUp} = props
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
+        const {setIsAuthorized, setSignUp} = props
+        const [username, setUsername] = useState("");
+        const [password, setPassword] = useState("");
 
-    function onClickSignIn() {
-        const requestOptions = {
-            method: 'POST',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({username: username, password: password})
-        };
-        fetch("api/token/", requestOptions).then(response => {
-            if (response.status > 400) {
-                return this.setState(() => {
-                    return {placeholder: "something went wrong!"};
-                });
-            }
-            return response.json()
-        }).then(data => {
-            cookie.set('token', data?.access);
-            setIsAuthorized(true)
-        });
+        function onClickSignIn() {
+            const requestOptions = {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({username: username, password: password})
+            };
+            fetch("api/token/", requestOptions).then(response => {
+                if (response.status > 400) {
+                    return this.setState(() => {
+                        return {placeholder: "something went wrong!"};
+                    });
+                }
+                return response.json()
+            }).then(data => {
+                cookie.set('token', data?.access);
+                setIsAuthorized(true)
+            });
+        }
+
+        return (
+            <Container component="main" maxWidth="xs">
+                <div style={{
+                        marginTop: 10, display: "flex", flexDirection: 'column',
+                        alignItems: 'center',
+                    }}>
+                    < Typography component="h1" variant="h5">
+                        Sign in
+                    </Typography>
+                    <form style={{width: "100%", marginTop: 1}} noValidate>
+                        <TextField
+                            onChange={(e) => setUsername(e.target.value)}
+                            value={username}
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="email"
+                            label="Email Address"
+                            name="email"
+                            autoComplete="email"
+                            autoFocus
+                        />
+                        <TextField
+                            onChange={(e) => setPassword(e.target.value)}
+                            value={password}
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="password"
+                            label="Password"
+                            type="password"
+                            id="password"
+                            autoComplete="current-password"
+                        />
+                        <Grid container spacing={2}>
+                            <Grid item xs={6}>
+                                <Button
+                                    fullWidth
+                                    variant="contained"
+                                    color="primary"
+                                    onClick={onClickSignIn}
+                                >
+                                    Sign In
+                                </Button>
+                            </Grid>
+                            <Grid item xs={6}>
+                                <Button
+                                    fullWidth
+                                    variant="contained"
+                                    color="primary"
+                                    onClick={() => setSignUp(true)}
+                                >
+                                    Sign Up
+                                </Button>
+                            </Grid>
+                        </Grid>
+                    </form>
+                </div>
+            </Container>
+        );
     }
-
-    return (
-        <>
-            <TextField
-                label="Username"
-                onChange={(e) => setUsername(e.target.value)}
-                value={username}
-            />
-            <TextField
-                label="Password"
-                onChange={(e) => setPassword(e.target.value)}
-                value={password}
-            />
-            <Button onClick={onClickSignIn}>Sign in</Button>
-            <Button onClick={()=>setSignUp(true)}>Sign up</Button>
-        </>
-
-    );
-};
+;
 export default SignIn;
